2d3ed73ef5536d3552cfe09d4ce1bc540e4d499d
Zapad82 <strana39@yandex.ru>
2025-01-13 11:30:50 +0300

    Решение задачи "Орел и Решка"
    Signed-off-by: Zapad82 <strana39@yandex.ru>

--- heads_and_tails.py
+++ heads_and_tails.py
@@ -12,5 +12,25 @@
 
 Примечание. Если выпавших Решек нет, то необходимо вывести число 0. """
 
+input_text = input()
+tail = "Р"
+counter_tails = 0
+max_tails = 0
+for char in input_text:
+    if char == tail:
+        counter_tails += 1
+    else:
+        if counter_tails > max_tails:
+            max_tails = counter_tails
+        counter_tails = 0
+    if counter_tails > max_tails:
+        max_tails = counter_tails
+
+print(max_tails)
+
+
+
+
+
 
 
